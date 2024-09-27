531579889bb24bbeaf88edc9288f322f77a3bddf
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2024-09-27 17:13:32 +0330

    :sparkles: sync or async? :)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use rocket::*;
+use std::{fs, io};
+use tokio::time::{sleep, Duration};
 // #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -11,7 +13,27 @@ fn hello() -> &'static str {
     "Hello, Rocket!"
 }
 
-#[launch]
-fn rocket() -> Rocket<Build> {
-    rocket::build().mount("/", routes![index, hello])
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+
+#[get("/blocking")]
+async fn blocking_task() -> io::Result<Vec<u8>> {
+    let vec = rocket::tokio::task::spawn_blocking(|| fs::read("data.txt"))
+        .await
+        .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, e))??;
+
+    Ok(vec)
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/", routes![index, hello, delay, blocking_task])
+        .launch()
+        .await?;
+
+    Ok(())
 }
