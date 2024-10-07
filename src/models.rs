96d79eecd86281713e8b8aa6dfa803e059795cfd
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2024-10-07 17:41:36 +0330

    :card_file_box: add users table and relate to to todos

--- src/models.rs
+++ src/models.rs
@@ -2,7 +2,19 @@ use crate::schema::*;
 use diesel::prelude::*;
 use std::time::SystemTime;
 
-#[derive(Queryable, Selectable)]
+#[derive(Queryable, Identifiable, Selectable, Debug, PartialEq)]
+#[diesel(table_name = users)]
+pub struct User {
+    pub id: i32,
+    pub name: String,
+    pub email: Option<String>,
+    pub password_hash: String,
+    pub created_at: SystemTime,
+    pub updated_at: Option<SystemTime>,
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
+#[diesel(belongs_to(User))]
 #[diesel(table_name = todos)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Todo {
@@ -10,6 +22,7 @@ pub struct Todo {
     pub title: String,
     pub body: String,
     pub completed: bool,
+    pub user_id: i32,
     pub created_at: SystemTime,
     pub updated_at: Option<SystemTime>,
 }
